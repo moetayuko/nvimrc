09f9eeb34bcc8280c00fae993e048b84c07e7475
MoetaYuko <loli@yuko.moe>
2023-10-16 22:10:10 +0800

    Update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.o.fileencodings = "ucs-bom,utf-8,gbk,gb18030,big5,euc-jp,latin1"
+
+local Util = require("lazyvim.util")
+if not Util.is_win() then
+  vim.g.python3_host_prog = "/usr/bin/python3"
+end
